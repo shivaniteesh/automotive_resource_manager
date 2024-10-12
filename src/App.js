8d0e6fb374a52e97b246a49c7dc2aa53fac0b89d
shivaniteesh <niteeshshivadhanapati4444.com>
2024-10-12 03:26:00 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+     <div className="about-us-container">
+      <p className="about-us-description">Welcome to ARM, where you can borrow rides!</p>
+      <p className="about-us-content">
+        At ARM we are passionate to serve you with proper functioning light weight vehicles. we tend to keep a close look on the vehicles to provide best services
+      </p>
+     
+      <p className="about-us-content">
+        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
+        Feel free to explore our collection, ask questions, and let us help you find the perfect vehicle fpr rent.
+      </p>
+      
+      <p className="about-us-content">
+        Join us in our mission to provide best service. have a best experience
+        Thank You!
+      </p>
+    </div>
+  );
+}
+
+export default App;
